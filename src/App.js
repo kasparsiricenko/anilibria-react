f09bd1e900bf4f886c118ecbf32146dfb0ba29ad
kasparsiricenko <kaspar.siricenko@gmail.com>
2023-10-30 07:56:23 +0000

    added first query

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import React from "react";
 import "./App.css";
 
+import { useQuery } from "react-query";
+
 function App() {
+  const { isLoading, error, data } = useQuery("teams", () => {
+    const formData = new FormData();
+    formData.append("query", "teams");
+    return fetch("http://localhost:3000/public/api/index.php", {
+      method: "POST",
+      body: new URLSearchParams(formData),
+    }).then((res) => res.json());
+  });
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
   return (
-    <div className="App">Anilibria Unofficial Web Alternative Application</div>
+    <div className="App">
+      Anilibria Unofficial Web Alternative Application
+      {JSON.stringify(data)}
+    </div>
   );
 }
 
