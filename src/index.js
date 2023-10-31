5cccabfe64af8dde46340485674b65d8bda631d8
kasparsiricenko <kaspar.siricenko@gmail.com>
2023-10-31 03:35:12 +0000

    remove unused useeffect

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
@@ -43,7 +43,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
